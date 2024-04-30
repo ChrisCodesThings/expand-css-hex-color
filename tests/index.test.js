5da288bc50058c57a3833744caf3ecd9c6aa915d
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-30 22:32:01 +0100

    tests: remove unnecessary tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,38 +1,19 @@
 
 const { default: testFunc } = await import("../");
 
-describe("check for bad input", () => {
-    test("nothing", async () => {
-        expect(testFunc()).toBeUndefined();
-    });
-
-    test("boolean", async () => {
-        expect(testFunc(true)).toBeUndefined();
-    });
-
-    test("a number", async () => {
-        expect(testFunc(123)).toBeUndefined();
-    });
-
-    test("not a css colour string", async () => {
-        expect(testFunc("foo")).toBeUndefined();
-    });
-});
-
 describe("expand some colours", () => {
     test("should not expand", async () => {
-        expect(testFunc("#123456")).toEqual("#123456");
-    });
-
-    test("should not expand", async () => {
+        expect(testFunc("#12345678")).toEqual("#12345678");
         expect(testFunc("#aabbccdd")).toEqual("#aabbccdd");
     });
 
-    test("should expand", async () => {
-        expect(testFunc("#abc")).toEqual("#aabbcc");
+    test("should add alpha", async () => {
+        expect(testFunc("#663399")).toEqual("#663399ff");
+        expect(testFunc("#aabbcc")).toEqual("#aabbccff");
     });
 
     test("should expand", async () => {
-        expect(testFunc("#dead")).toEqual("#ddeeaadd");
+        expect(testFunc("#abc")).toEqual("#aabbccff");
+        expect(testFunc("#f00f")).toEqual("#ff0000ff");
     });
 });
